0eaba8bb417a74e6d3843d9456487430dd471132
andres1006 <andresagudelo1006@gmail.com>
2020-05-04 14:20:53 -0500

    actualizacion main

--- main.cpp
+++ main.cpp
@@ -19,30 +19,6 @@
 
 using namespace std;
 
-void list_dir(string dir)
-{
-  DIR * directorio;
-  struct dirent * elemento;
-  string elem;
-  if (directorio = opendir(dir.c_str()))
-  {
-   while (elemento = readdir(directorio))
-   {
-    elem = elemento->d_name;
-    if (elem != "." && elem != "..") cout << elem << endl;
-   }
-  }
-  closedir(directorio);
-}
-
-void init(string pathalistar)
-{
-  cout << "Ruta del directorio a listar: ";
-  string dir;
-  getline(cin, dir);
-  list_dir(pathalistar);
-  init(pathalistar);
-}
 
 int main(int argc, char *argv[])
 {
@@ -97,7 +73,7 @@ Prueba subida datos a Mongo DB: los archivos necesarios para esta parte son: log
     data->setTstFileName(argv[2]); // datos de la prueba
     data->open(0);
     data->importCsv();
-    qDebug()<<"m_processedData.size() =";
+    //qDebug()<<"m_processedData.size() =";
     //qDebug()<<data->dataSize();
     procesamiento->sendToDB(data,argv[3],argv[4],"localhost",27017);// argv 3 => nombre del paciente, argv 4 nombre de la prueba
 
